dcd950eb533fd628bff92d0f483ea98a342b15e3
Chaudhary Shivam (BGSW/ERD1) <Shivam.Chaudhary@in.bosch.com>
2024-06-01 21:09:21 +0530

    Config modifications

--- configs/model_configs/DeepLabv1.py
+++ configs/model_configs/DeepLabv1.py
@@ -136,7 +136,7 @@ def DeepLabv1Config(cfg):
     cfg.DeepLabv1.LOSS.PARAMS.dice_params.normalize = False
     cfg.DeepLabv1.LOSS.PARAMS.dice_params.smooth = 0.0
     cfg.DeepLabv1.LOSS.PARAMS.dice_params.classes = None
-    # cfg.DeepLabv1.LOSS.PARAMS.class_weightage_method = "inverse_frequency"
+    cfg.DeepLabv1.LOSS.PARAMS.class_weightage_method = "inverse_frequency"
 
     cfg.REGULARIZATION = CN()
     cfg.REGULARIZATION.MODE = ''
