2c638de0c80c37893a5d2c2bad1fbc612f07dc93
Chaudhary Shivam (BGSW/ERD1) <Shivam.Chaudhary@in.bosch.com>
2024-06-10 11:35:36 +0530

    Added warmup poly lr scheduler

--- src/custom_lrs.py
+++ src/custom_lrs.py
@@ -88,3 +88,37 @@ class PolynomialLRScheduler(_LRScheduler):
                          self.end_learning_rate for base_lr in self.base_lrs]
             for param_group, lr in zip(self.optimizer.param_groups, decay_lrs):
                 param_group["lr"] = lr
+
+class WarmUpPolyLRScheduler(_LRScheduler):
+
+    def __init__(self, optimizer, after_scheduler, lr_initial, lr_final, warmup_epochs, warmup_method="linear", last_epoch=-1):
+
+        self.warmup_epochs = warmup_epochs
+        self.c = lr_initial
+        self.a = (lr_final - self.c) / (warmup_epochs ** 2)
+        self.b = (lr_final - self.a * warmup_epochs ** 2 - self.c) / warmup_epochs
+        self.after_scheduler = after_scheduler
+
+        self.intercept = lr_initial
+        self.slope = (lr_final - lr_initial) / warmup_epochs
+
+        self.warmup_method = warmup_method
+
+        super(WarmUpPolyLRScheduler, self).__init__(optimizer=optimizer, last_epoch=last_epoch)
+
+    def get_lr(self):
+        epoch = self.last_epoch
+        if epoch <= self.warmup_epochs:
+            if self.warmup_method == "linear":
+                lr = self.slope * epoch + self.intercept
+            elif self.warmup_method == "polynomial":
+                lr = self.a * (epoch ** 2) + self.b * epoch + self.c
+            return [max(lr, 0) for _ in self.optimizer.param_groups]
+        else:
+            return self.after_scheduler.get_lr()
+
+    def step(self):
+        if self.last_epoch <= self.warmup_epochs:
+            super(WarmUpPolyLRScheduler, self).step()
+        if self.last_epoch >= self.warmup_epochs:
+            self.after_scheduler.step()
