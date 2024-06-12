1d31f437e6a862620e1056344009c007b9745ab3
Chaudhary Shivam (BGSW/ERD1) <Shivam.Chaudhary@in.bosch.com>
2024-06-12 15:36:02 +0530

    Classifications Performance Eval Script

--- phases/classification/performance_eval.py
+++ phases/classification/performance_eval.py
@@ -2,12 +2,14 @@ import time
 import torch
 import backbones
 from src.gpu_devices import GPU_Support
-from src.metrics import AverageMeter, ClassificationMetrics, ProgressMeter
+from src.metrics import ClassificationMetrics
+from utils.logging_utils import AverageMeter, ProgressMeter
 from src.checkpoints import Checkpoint
 import configs
 from torchvision import transforms as T
 from torch.utils.data import DataLoader
 from configs.config import setup_config
+import torch.nn.functional as F
 from datasets.classification.dataset import ClassificationDataset
 from utils.global_params import Global
 
@@ -25,6 +27,9 @@ class Evaluate:
         self._loadDataloader()
         self._loadModel()
         self._loadCriterion()
+        self.metrics = ClassificationMetrics(num_classes=1000)
+
+        print(f"\n**Evaluating {architecture} on ImageNet Validation Data**\n")
 
     def _loadCriterion(self):
         self.criterion = getattr(
@@ -55,6 +60,7 @@ class Evaluate:
         cfg = setup_config(default=True)
         GPU_Support.set_gpu_devices("0", log=False)
 
+        cfg.num_gpus = 1
         getattr(configs, ''.join([self.architecture, "Config"]))(cfg)
 
         self.cfg = cfg
@@ -89,12 +95,16 @@ class Evaluate:
         
         with torch.no_grad():
             end = time.time()
+            epoch_loss = 0
             for i, (images, target) in enumerate(self.dataloader):
                 images = images.cuda()
                 target = target.cuda()
 
                 output = self.model(images)
                 loss = self.criterion(output, target)
+                epoch_loss += loss
+                predicted_classes = torch.argmax(F.softmax(output, dim=1), dim=1)
+                self.metrics.update(target.cpu().numpy(), predicted_classes.cpu().numpy())
 
                 acc1, acc5 = ClassificationMetrics.topKaccuracy(output, target, topk=(1, 5))
                 losses.update(loss.item(), images.size(0))
@@ -104,16 +114,32 @@ class Evaluate:
                 batch_time.update(time.time() - end)
                 end = time.time()
 
-                if i % 100 == 0:
+                if i % 50 == 0:
                     progress.display(i, write=True)
 
+            self.metrics.aggregate_metrics()
+
+            epoch_loss /= len(self.dataloader)
+
             with open(progress.report_path, "a") as f:
-                f.write("\n" + ' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'.format(top1=top1, top5=top5) + "\n")
+                f.write("\n" + '\t** Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f} Precision {precision:.3f} Recall {recall:.3f} F1 Score {f1_score:.3f}'.format(
+                top1=top1,
+                top5=top5,
+                precision=self.metrics.metrics_aggregated['precision'],
+                recall=self.metrics.metrics_aggregated['recall'],
+                f1_score=self.metrics.metrics_aggregated['f1_score']
+            ) + "\n")
             
-            print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
+            print('\t** Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f} Precision {precision:.3f} Recall {recall:.3f} F1 Score {f1_score:.3f}'.format(
+                top1=top1,
+                top5=top5,
+                precision=self.metrics.metrics_aggregated['precision'],
+                recall=self.metrics.metrics_aggregated['recall'],
+                f1_score=self.metrics.metrics_aggregated['f1_score']
+            ))
 
         return top1.avg
 
 
-metric = Evaluate(architecture="ViT")
+metric = Evaluate(architecture="ConvNeXt")
 metric.validate()
