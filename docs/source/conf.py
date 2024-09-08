2f3f40339d00456d8086eb14dc6fa0563b0dff05
Chaudhary Shivam (BGSW/ERD1) <Shivam.Chaudhary@in.bosch.com>
2024-09-08 20:11:59 +0530

    doc testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_material"
+html_theme = "sphinx_rtd_theme"
 
 html_static_path = ['_static']
 html_show_sphinx = False
