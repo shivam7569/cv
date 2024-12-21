1b2945e06b0d6a50a11e6bedaaed1e008612e508
Chaudhary Shivam (BGSW/ERD1) <Shivam.Chaudhary@in.bosch.com>
2024-12-22 00:30:06 +0530

    Removed the redundancy by incorporating package modules

--- cv/contrastive/clip/model.py
+++ cv/contrastive/clip/model.py
@@ -1,14 +1,19 @@
 import torch
 import numpy as np
 import torch.nn as nn
-from functools import partial
+from functools import lru_cache, partial
 
-from cv.utils import Global
+from cv.backbones import ViT
+from cv.utils import MetaWrapper
 from cv.src.tokenizer import SimpleTokenizer, tokenize
 from cv.attention.transformers import TransformerBlock
 
 
-class Clip(nn.Module):
+class Clip(nn.Module, metaclass=MetaWrapper):
+
+    @classmethod
+    def __class_repr__(cls):
+        return "Model Class for CLIP architecture from paper on: Learning Transferable Visual Models From Natural Language Supervision"
 
     def __init__(
             self,
@@ -20,7 +25,9 @@ class Clip(nn.Module):
             vit_num_heads,
             vit_num_blocks,
             text_num_heads,
+            text_num_blocks,
             context_dim,
+            vit_dropout,
             encoder_dropout,
             attention_dropout,
             projection_dropout,
@@ -35,49 +42,35 @@ class Clip(nn.Module):
         
         super(Clip, self).__init__()
 
-        Clip._assertions(image_size=image_size, patch_size=patch_size, patchify_technique=patchify_technique)
+        if stodepth:
+            drop_probs = [stochastic_depth_mp / (text_num_blocks - 1) * i for i in range(text_num_blocks)]
+        else:
+            drop_probs = [0.0 for _ in range(text_num_blocks)]
 
-        self.patch_size = patch_size
-        self.in_channels = in_channels
         self.embed_dim = embed_dim
         self.context_dim = context_dim
-        self.vit_num_blocks = vit_num_blocks
+        self.text_num_blocks = text_num_blocks
 
-        scale = embed_dim ** -0.5
-        num_patches = (image_size // patch_size) ** 2
-
-        if stodepth:
-            drop_probs = [stochastic_depth_mp / (vit_num_blocks - 1) * i for i in range(vit_num_blocks)]
-        else:
-            drop_probs = [0.0 for _ in range(vit_num_blocks)]
-        
-        self.image_encoder = nn.Sequential(*[
-            TransformerBlock(
-                embed_dim=embed_dim, d_ff=d_ff, num_heads=vit_num_heads, encoder_dropout=encoder_dropout,
-                attention_dropout=attention_dropout, projection_dropout=projection_dropout,
-                stodepth_prob=drop_probs[i], ln_order=ln_order, layer_scale=layer_scale
-            ) for i in range(vit_num_blocks)
-        ])
-
-        self.patchify = self._linear_patchify if patchify_technique == "linear" else self._conv_patchify
-
-        self.class_token = nn.Parameter(
-            scale * torch.rand((1, 1, embed_dim)), requires_grad=True
+        self.vit = ViT(
+            num_classes=None, d_model=embed_dim, image_size=image_size, patch_size=patch_size,
+            classifier_mlp_d=None, encoder_mlp_d=d_ff, encoder_num_heads=vit_num_heads,
+            num_encoder_blocks=vit_num_blocks, dropout=vit_dropout, encoder_dropout=encoder_dropout,
+            encoder_attention_dropout=attention_dropout, encoder_projection_dropout=projection_dropout,
+            patchify_technique=patchify_technique, stochastic_depth=stodepth, stochastic_depth_mp=stochastic_depth_mp,
+            layer_scale=layer_scale, ln_order=ln_order, in_channels=in_channels, classifier=False
         )
-        self.img_position_embedding = nn.Embedding(num_embeddings=num_patches + 1, embedding_dim=embed_dim)
-        self.register_buffer(
-            "img_position_ids",
-            torch.arange(num_patches + 1).expand(1, -1),
-            persistent=False
-        )
-        self.img_ln = nn.LayerNorm(normalized_shape=embed_dim)
+
         self.img_embed_proj = nn.Linear(in_features=embed_dim, out_features=proj_dim, bias=False)
 
-        self.text_encoder = TransformerBlock(
-            embed_dim=embed_dim, d_ff=d_ff, num_heads=text_num_heads, encoder_dropout=encoder_dropout,
-            attention_dropout=attention_dropout, projection_dropout=projection_dropout,
-            ln_order=ln_order, layer_scale=layer_scale
-        )
+        self.text_encoder = [
+            TransformerBlock(
+                embed_dim=embed_dim, d_ff=d_ff, num_heads=text_num_heads,
+                encoder_dropout=encoder_dropout, attention_dropout=attention_dropout,
+                projection_dropout=projection_dropout, ln_order=ln_order,
+                stodepth_prob=drop_probs[i], layer_scale=layer_scale
+            ) for i in range(text_num_blocks)
+        ]
+        
         self.vocab_size = SimpleTokenizer().vocab_size
         self.text_tokenizer = partial(tokenize, context_length=context_dim, truncate=False)
         self.text_embeddings = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=embed_dim)
@@ -94,38 +87,16 @@ class Clip(nn.Module):
 
         self._initialize()
 
-    @staticmethod
-    def _assertions(image_size, patch_size, patchify_technique):
-        assert image_size % patch_size == 0, Global.LOGGER.error(f"Patch size {patch_size} is not a divisor of image dimension {image_size}")
-        assert patchify_technique in ("linear", "convolutional"), Global.LOGGER.error(f"{patchify_technique} patchify technique not supported")
-
-    def _linear_patchify(self, img):
-        linear_projection = nn.functional.unfold(
-            input=img, kernel_size=(self.patch_size, self.patch_size), stride=self.patch_size
-        )
-        linear_projection = linear_projection.transpose(dim0=1, dim1=2)
-
-        return linear_projection
-
-    def _conv_patchify(self, img):
-        conv_projection = nn.Conv2d(
-            in_channels=self.in_channels, out_channels=self.embed_dim,
-            kernel_size=self.patch_size, stride=self.patch_size
-        )(img)
-        conv_projection = torch.flatten(input=conv_projection, start_dim=2)
-        conv_projection = conv_projection.transpose(dim0=1, dim1=2) # shape: (batch_size, num_patches, embedding_size)
-
-        return conv_projection
-
     def _initialize(self):
-        nn.init.normal_(self.text_embeddings.weight, std=0.02)
-        nn.init.normal_(self.txt_position_embedding.weight, std=0.01)
-
         attn_std = self.embed_dim ** -0.5
         fc_std = (2 * self.embed_dim) ** -0.5
-        proj_std = (self.embed_dim ** -0.5) * ((2 * self.vit_num_blocks) ** -0.5)
+        proj_std = (self.embed_dim ** -0.5) * ((2 * self.text_num_blocks) ** -0.5)
 
-        for block in self.image_encoder:
+        nn.init.normal_(self.text_embeddings.weight, std=0.02)
+        nn.init.normal_(self.txt_position_embedding.weight, std=0.01)
+        nn.init.normal_(self.txt_embed_proj.weight, std=self.embed_dim ** -0.5)
+
+        for block in self.text_encoder:
             nn.init.normal_(block.msa.w_q.weight, std=attn_std)
             nn.init.normal_(block.msa.w_k.weight, std=attn_std)
             nn.init.normal_(block.msa.w_v.weight, std=attn_std)
@@ -133,8 +104,7 @@ class Clip(nn.Module):
             nn.init.normal_(block.mlp[0].weight, std=fc_std)
             nn.init.normal_(block.mlp[-1].weight, std=proj_std)
 
-        nn.init.normal_(self.txt_embed_proj.weight, std=attn_std)
-
+    @lru_cache
     def _build_mask(self):
         mask = torch.ones(self.context_dim, self.context_dim)
         mask.triu_(1)
@@ -142,11 +112,7 @@ class Clip(nn.Module):
         return mask.bool()
     
     def _encode_img(self, img):
-        img_tokens = self.patchify(img)
-        img_tokens = torch.cat([self.class_token.expand(img.size(0), -1, -1), img_tokens], dim=1)
-        img_tokens = img_tokens + self.img_position_embedding(self.img_position_ids)
-        img_embeddings = self.image_encoder(img_tokens)
-        img_embeddings = self.img_ln(img_embeddings[:, 0, :])
+        img_embeddings = self.vit(img)
         img_embeddings = self.img_embed_proj(img_embeddings)
 
         return img_embeddings
@@ -155,7 +121,13 @@ class Clip(nn.Module):
         tokenized_texts = self.text_tokenizer(txt)
         txt_tokens = self.text_embeddings(tokenized_texts)
         txt_tokens = txt_tokens + self.txt_position_embedding(self.txt_position_ids)
-        txt_embeddings = self.text_encoder(txt_tokens, mask=self._build_mask())
+
+        for idx, encoder_block in enumerate(self.text_encoder):
+            if idx == 0:
+                txt_embeddings = encoder_block(txt_tokens, mask=self._build_mask())
+            else:
+                txt_embeddings = encoder_block(txt_embeddings, mask=self._build_mask())
+
         txt_embeddings = self.txt_ln(txt_embeddings)[
             torch.arange(txt_embeddings.shape[0]), tokenized_texts.argmax(dim=-1)
         ]
@@ -175,3 +147,11 @@ class Clip(nn.Module):
         logits_per_text = logits_per_image.t()
 
         return (logits_per_image, logits_per_text)
+
+clp = Clip(224, 16, 768, 512, 768*4, 12, 12, 8, 12, 77, 0.0, 0.0, 0.0, 0.0, 0.07, "pre", True, 0.1, 1e-6, "convolutional", 3)
+x = torch.randn(2, 3, 224, 224)
+t = ["a horse", "the cat"]
+a, b = clp(x, t)
+
+print(a)
+print(b)
